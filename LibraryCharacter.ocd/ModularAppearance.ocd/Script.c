18d75c1f2b1db4eee6f2492d3d6a49c6198b621e
Mark <markh4b@hotmail.com>
2017-04-12 06:31:03 +0200

    Fix UTF-8 warnings

--- LibraryCharacter.ocd/ModularAppearance.ocd/Script.c
+++ LibraryCharacter.ocd/ModularAppearance.ocd/Script.c
@@ -8,13 +8,13 @@
  */
  
 // Was sollte das Zeug haben?
-// - Slots für die einzelnen Teile
+// - Slots fuer die einzelnen Teile
 // - man kann die Slots mit einer Fallback-Grafik belegen, die angezeigt wird, wenn man nichts benutzt
 // - ein Slot zeigt entweder die aktuelle Grafik an, oder die Fallback-Grafik, falls vorhanden, oder nichts
 // - die aktuelle Grafik / Attachment-ID des Meshes sollte in einem Array gespeichert sein
 // - evtl. sollte das attachde Objekt im Array enthalten sein
-// - wenn ein Objekt attached wird, dann sollte die Mesh-Grafik in diesem Objekt abgefragt werden? Oder das Objekt fragt beim Ziel an, ob es einen Skin für diese Grafik hat. Wahrscheinlich ist letzteres besser. Ansonsten eben die Defaul-Grafik
-// - Priorität ist explizit nicht notwendig, es kann einfach nur ein Ding je Slot verwendet werden.
+// - wenn ein Objekt attached wird, dann sollte die Mesh-Grafik in diesem Objekt abgefragt werden? Oder das Objekt fragt beim Ziel an, ob es einen Skin fuer diese Grafik hat. Wahrscheinlich ist letzteres besser. Ansonsten eben die Defaul-Grafik
+// - Prioritaet ist explizit nicht notwendig, es kann einfach nur ein Ding je Slot verwendet werden.
 // - wahrscheinlich sollte in dem Array jeweils eine Proplist enthalten sein mit {definition, attachmentID, graphicsName, mesh material usw?}
 
 static const LIB_APPEARANCE_SLOT_MAIN = 0;
